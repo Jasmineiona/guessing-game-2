3844086a74ad494dd711a4455aa9468e9d368ca8
Robert Hill <codingbyrobert@gmail.com>
2015-09-30 12:52:28 -0700

    This is the first version

---
+++ game.js
@@ -0,0 +1,14 @@
+var question1 = prompt("Does Robert have any tattoos?");
+console.log('Answer to question1 "tattoos" '+ question1);
+
+
+var question2 = prompt('Does Robert have any pets?');
+console.log('Answer to question2 "Pets" ' + question2);
+
+
+var question3 = prompt('Does Robert hate camping?');
+console.log('Answer to question3 " Camping" ' + question3);
+
+alert('Your answer was ' + question1 + ' to the question about tattoos');
+alert('Your answer was ' + question2 + ' to the question about pets.');
+alert('Your answer was ' + question3 + ' to the question about camping.');
