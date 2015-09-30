b388335664f21bf4c69650d16f9421fe453eaef8
Robert Hill <codingbyrobert@gmail.com>
2015-09-30 13:59:14 -0700

    Error correction done and revamped last question

--- game.js
+++ game.js
@@ -1,39 +1,44 @@
 var playerName = prompt('What is your name?');
-alert('Thank you ' + playerName + ' please answer the following questions with a "yes" or "no"');
+console.log('The players name is ' + playerName);
+
+alert('Thank you ' + playerName + ' please answer the following questions with a "yes" or "no" or with "y" or "n"');
 //here for furture use
 //var score = 0;
 
 var question1 = prompt("Does Robert have any tattoos?");
-console.log('Answer to question1 "tattoos" '+ question1);
 
-if (question1 == 'yes') {
+if (question1.toLowerCase() == 'yes' || 'y') {
   alert('Good job ' + playerName + ' you got that one right. He has a lot of tattoos');
   //score += 1;
 
 } else{
 alert('Sorry ' + playerName +' you got that wrong. Robert has more tattoos than he can count');
 }
+console.log('Answer to question1 "tattoos" '+ question1);
+
 
 var question2 = prompt('Does Robert have any pets?');
-console.log('Answer to question2 "Pets" ' + question2);
 
-if (question2 == 'yes') {
+if (question2.toLowerCase == 'yes' || 'y') {
 alert('Good guess ' + playerName + ' he does have a black lab');
 // score += 1;
 } else{
   alert('Nope ' + playerName + ' He does have a dog');
 }
+console.log('Answer to question2 "Pets" ' + question2);
 
 
+var question3 = prompt('Does Robert love camping?');
 
-var question3 = prompt('Does Robert hate camping?');
-console.log('Answer to question3 " Camping" ' + question3);
-if (question3 == "yes") {
-alert('Sorry ' + playerName + ' He is happiest when he is in the woods');
+if (question3.toLowerCase() == "yes" || 'y') {
+alert('Correct ' + playerName + ' he will go camping anytime of year and in any weather.');
+// score +=1;
 } else{
- alert('Correct ' + playerName + ' he will go camping anytime of year and in any weather.');
- // score +=1;
+  alert('Sorry ' + playerName + ' He is happiest when he is in the woods');
+
+
 }
+console.log('Answer to question3 " Camping" ' + question3);
 
 
 
