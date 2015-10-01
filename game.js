5ccfb2b8e0bd27c7e39108b2855bc4e2256ff39c
Emily Friedman <emilyfriedman@Emilys-MacBook-Pro.local>
2015-10-01 14:14:20 -0700

    moved alerts to DOM

--- game.js
+++ game.js
@@ -11,11 +11,11 @@ function q1 () {
 	var question1 = prompt("Does Robert have any tattoos?");
 
 if (question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y'){
-  alert('Good job ' + playerName + ' you got that one right. He has a lot of tattoos');
+  one.innerHTML = 'Good job ' + playerName + ' you got that one right. He has a lot of tattoos';
   score += 1;
 }
 else{
-  alert('Sorry ' + playerName + ' you got that wrong. Robert has more tattoos than he can count');
+  one.innerHTML = 'Sorry ' + playerName + ' you got that wrong. Robert has more tattoos than he can count';
 }
 
 console.log('Answer to question1 "tattoos" '+ question1);
@@ -25,25 +25,25 @@ console.log('Answer to question1 "tattoos" '+ question1);
 function q2 () {
 var question2 = prompt('Does Robert have any pets?');
 
-if (question2.toLowerCase === 'yes' || question2.toLowerCase() === 'y'){
-  alert('Good guess ' + playerName + ' he does have a black lab');
+if (question2.toLowerCase()=== 'yes' || question2.toLowerCase() === 'y'){
+  two.innerHTML = 'Good guess ' + playerName + ' he does have a black lab';
   score += 1;
 }
 else{
-  alert('Nope ' + playerName + ' He does have a dog');
+  two.innerHTML = 'Nope ' + playerName + ' He does have a dog';
 }
 console.log('Answer to question2 "Pets" ' + question2);
 }
 
-function q2 () {
+function q3 () {
 var question3 = prompt('Does Robert love camping?');
 
 if (question3.toLowerCase() === "yes" || question3.toLowerCase() === 'y'){
-alert('Correct ' + playerName + ' he will go camping anytime of year and in any weather.');
+three.innerHTML = 'Correct ' + playerName + ' he will go camping anytime of year and in any weather.';
  score +=1;
 }
 else{
-  alert('Sorry ' + playerName + ' He is happiest when he is in the woods');
+  three.innerHTML = 'Sorry ' + playerName + ' He is happiest when he is in the woods';
 }
 console.log('Answer to question3 "Camping" ' + question3);
 }
@@ -53,10 +53,10 @@ q2();
 q3();
 
 if (score >= 3){
-  alert('Great job ' + playerName +' you got them all right. Thanks for playing.');
+  total.innerHTML = 'Great job ' + playerName +' you got them all right. Thanks for playing.';
 }
 else{
-  alert(playerName + ' you got ' + score +' out of 3 right. Thanks for playing.');
+  total.innerHTML = playerName + ' you got ' + score +' out of 3 right. Thanks for playing.';
 }
 
 
